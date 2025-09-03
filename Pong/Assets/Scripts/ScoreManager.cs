e6f85762e2e6912dd67b1821b3ab0a04162bfef5
Derpamon <ryananimates0803@gmail.com>
2025-09-02 20:30:21 -0500

    Auto Ball Spawn

--- Pong/Assets/Scripts/ScoreManager.cs
+++ Pong/Assets/Scripts/ScoreManager.cs
@@ -13,6 +13,12 @@ public class ScoreManager : MonoBehaviour
     public int maxScore;
     public TextMeshProUGUI p1ScoreText;
     public TextMeshProUGUI p2ScoreText;
+    public TextMeshProUGUI winText;
+
+    public string winMessage;
+
+    public List<string> p1WinMessages;
+    public List<string> p2WinMessages;
 
     private void Awake()
     {
@@ -30,27 +36,51 @@ public class ScoreManager : MonoBehaviour
         {
             p1Score += 1;
         }
-        else 
+        else
         {
             p2Score += 1;
         }
 
+        if (p1Score >= maxScore || p2Score >= maxScore)
+        {
+            HandleWin(isP1);
+        }
+        else
+        {
+            BallSpawner.Instance.SpawnBall(isP1);
+        }
+
         UpdateScoreboard();
-    }
 
-    void UpdateScoreboard()
-    {
-        p1ScoreText.SetText(p1Score.ToString());
-        p2ScoreText.SetText(p2Score.ToString());
-    }
+        maxScore = 6;
 
-    void HandleWin(bool isP1)
-    {
+        void UpdateScoreboard()
+        {
+            p1ScoreText.SetText(p1Score.ToString());
+            p2ScoreText.SetText(p2Score.ToString());
+        }
 
-    }
+        void HandleWin(bool isP1)
+        {
+            string messageToDisplay;
 
-    void ResetGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            if (isP1)
+            {
+                int rand = Random.Range(0, p1WinMessages.Count);
+                messageToDisplay = p1WinMessages[rand];
+            }
+            else
+            {
+                int rand = Random.Range(0, p2WinMessages.Count);
+                messageToDisplay= p2WinMessages[rand];
+            }
+
+            winText.SetText(messageToDisplay);
+        }
+
+        void ResetGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
