e3547d45b22ff6015c9679772284c756ac3560c6
WUStudent <wustudent@webster.edu>
2025-09-02 20:30:16 -0500

    Auto Ball Spawn

--- Pong/Assets/Scripts/ScoreManager.cs
+++ Pong/Assets/Scripts/ScoreManager.cs
@@ -13,6 +13,10 @@ public class ScoreManager : MonoBehaviour
     public int maxScore;
     public TextMeshProUGUI p1ScoreText;
     public TextMeshProUGUI p2ScoreText;
+    public TextMeshProUGUI winText;
+
+    public List<string> p1WinMessages;
+    public List<string> p2WinMessages;
 
     private void Awake()
     {
@@ -35,6 +39,15 @@ public class ScoreManager : MonoBehaviour
             p2Score += 1;
         }
 
+        if (p1Score >= maxScore || p2Score >= maxScore)
+        {
+            HandleWin(isP1);
+        }
+        else
+        {
+            BallSpawner.Instance.SpawnBall(isP1);
+        }
+
         UpdateScoreboard();
     }
 
@@ -46,7 +59,20 @@ public class ScoreManager : MonoBehaviour
 
     void HandleWin(bool isP1)
     {
+        string messageToDisplay;
 
+        if (isP1)
+        {
+            int rand = Random.Range(0, p1WinMessages.Count);
+            messageToDisplay = p1WinMessages[rand];
+        }
+        else 
+        {
+            int rand = Random.Range(0, p2WinMessages.Count);
+            messageToDisplay = p2WinMessages[rand];
+        }
+
+       winText.SetText(messageToDisplay);
     }
 
     void ResetGame()
