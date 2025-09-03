bede25a82f79a086a566d63652612fbe6df70ea4
HLandfill-623 <159385699+hecksgirl@users.noreply.github.com>
2025-09-02 20:30:17 -0500

    auto ball spawn

--- Pong/Assets/Scripts/BallSpawner.cs
+++ Pong/Assets/Scripts/BallSpawner.cs
@@ -17,27 +17,45 @@ public class BallSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnBall();
+        SpawnBall(true);
     }
 
     // Update is called once per frame
-    void Update()
+   // void Update()
+   // {
+   //     if (Input.GetKeyDown(KeyCode.Space))
+    //    { 
+      //      SpawnBall();
+     //   }
+  //  }
+
+    public void SpawnBall(bool isP1)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        { 
-            SpawnBall();
-        }
+       StartCoroutine(DelaySpawnBall(isP1));
     }
 
-    public void SpawnBall()
+    private IEnumerator DelaySpawnBall(bool isP1)
     {
+
+        yield return new WaitForSeconds(3);
+
         Debug.Log("Spawn Ball");
         GameObject newBall = Instantiate(ballPrefab);
 
-        float randX = Random.Range(-1.0f, 1.0f);
+        float xDirection;
+        if (isP1)
+        {
+            xDirection = -1;
+        }
+        else
+        {
+            xDirection = 1;
+        }
+
         float randY = Random.Range(-1.0f, 1.0f);
-        spawnDirection = new Vector3(randX, randY, 0);
+        spawnDirection = new Vector3(xDirection, randY, 0);
 
         newBall.GetComponent<Ball>().direction = spawnDirection.normalized;
     }
+
 }
--- Pong/Assets/Scripts/ScoreManager.cs
+++ Pong/Assets/Scripts/ScoreManager.cs
@@ -13,6 +13,12 @@ public class ScoreManager : MonoBehaviour
     public int maxScore;
     public TextMeshProUGUI p1ScoreText;
     public TextMeshProUGUI p2ScoreText;
+    public TextMeshProUGUI winText;
+    
+    public string winMessage;
+
+    public List<string> p1WinMessages;
+    public List<string> p2WinMessages;
 
     private void Awake()
     {
@@ -35,6 +41,16 @@ public class ScoreManager : MonoBehaviour
             p2Score += 1;
         }
 
+
+        if(p1Score >= maxScore || p2Score >= maxScore)
+        {
+            HandleWin(isP1);
+        }
+        else
+        {
+            BallSpawner.Instance.SpawnBall(isP1);
+        }
+
         UpdateScoreboard();
     }
 
@@ -46,7 +62,20 @@ public class ScoreManager : MonoBehaviour
 
     void HandleWin(bool isP1)
     {
+        string messageToDisplay;
 
+        if (isP1)
+        {
+            int rand = Random.Range(0, p1WinMessages.Count);
+            messageToDisplay = p1WinMessages[rand];
+        }
+
+        else
+        {
+            int rand = Random.Range(0, p2WinMessages.Count);
+            messageToDisplay = p1WinMessages[rand];
+        }
+        winText.SetText(messageToDisplay); 
     }
 
     void ResetGame()
