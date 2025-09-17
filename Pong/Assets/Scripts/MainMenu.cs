9f915d8d2bb55d883dc22c085c1452a4f0a1e603
HLandfill-623 <hinc2142@gmail.com>
2025-09-16 20:07:29 -0500

    main menu added

---
+++ Pong/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Button startButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(HandleStartButtonPressed);
+    }
+
+    public void HandleStartButtonPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
