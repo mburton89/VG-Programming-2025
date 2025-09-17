59f92d452dffd8fe77523a733cf198ff2a70981e
WUStudent <161900780+Italianpianokey@users.noreply.github.com>
2025-09-16 20:06:59 -0500

    Main Menu

---
+++ Pong/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startButton;
+    public Scene pongGameplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(HandleStartButtonPressed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HandleStartButtonPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
