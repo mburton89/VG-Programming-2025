3c3f0c28cdfb2f550aad1955b46ca9705b186265
kommonkrow <w43t3rbr0g4@proton.me>
2025-09-16 21:06:07 -0500

    corrected script name typo

--- VacPac Adventures/Assets/Prototyping/Slime/scripts/IdleWander.cs
+++ VacPac Adventures/Assets/Prototyping/Slime/scripts/IdleWander.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleWandor : MonoBehaviour
+public class IdleWander : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
