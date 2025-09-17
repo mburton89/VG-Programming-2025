bfcac7b2327b18746a684ce1d3df724de2e2b30d
shadicl <itshard532@gmail.com>
2025-09-16 21:18:55 -0500

    Movement and Sprint Implementation
    a

---
+++ VacPac Adventures/Assets/Prototyping/FPSPlayer/Movement.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float mouseSensitivity = 2.0f;
+    public float jumpForce = 5.0f;
+    public Transform playerCamera;
+
+    private float originSpeed;
+
+    private CharacterController characterController;
+    private float verticalRotation = 0;
+    private float verticalVelocity = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            characterController = GetComponent<CharacterController>();
+            originSpeed = moveSpeed;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Camera
+        float horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, horizontalRotation, 0);
+
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
+        playerCamera.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        // Movement
+        float moveForward = Input.GetAxis("Vertical") * moveSpeed;
+        float moveSideways = Input.GetAxis("Horizontal") * moveSpeed;
+
+        Vector3 movement = new Vector3(moveSideways, verticalVelocity, moveForward);
+        movement = transform.rotation * movement;
+
+        // Jumping
+        if (characterController.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                print("jump");
+                verticalVelocity = jumpForce;
+                moveSpeed = originSpeed;
+            }
+        }
+        else
+        {
+            verticalVelocity -= Time.deltaTime * 10.0f; // Apply gravity
+        }
+
+        characterController.Move(movement * Time.deltaTime);
+
+        //Sprinting
+
+        if (characterController.isGrounded)
+        {
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                print("Sprint");
+                moveSpeed *= 2;
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                print("No sprint");
+                moveSpeed = originSpeed;
+            }
+        }
+    }
+}
