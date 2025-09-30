9c33f015979056e30addcb4480dd670b73e90aab
WUStudent <wustudent@webster.edu>
2025-09-30 17:38:19 -0500

    commented out broken code

--- VacPac Adventures/Assets/Scripts/EnemyDeath.cs
+++ VacPac Adventures/Assets/Scripts/EnemyDeath.cs
@@ -1,23 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class EnemyDeath : MonoBehaviour
-{
-    public EnemyAI enemyAI;
+//public class EnemyDeath : MonoBehaviour
+//{
+//    public EnemyAI enemyAI;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-            enemyAI.OnPlayerEnter(other.transform);
-    }
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//            enemyAI.OnPlayerEnter(other.transform);
+//    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-            enemyAI.OnPlayerExit();
-    }
+//    private void OnTriggerExit(Collider other)
+//    {
+//        if (other.CompareTag("Player"))
+//            enemyAI.OnPlayerExit();
+//    }
 
 
 
-}
+//}
--- VacPac Adventures/Assets/Scripts/GameOver.cs
+++ VacPac Adventures/Assets/Scripts/GameOver.cs
@@ -1,37 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class GameOver : MonoBehaviour
-{
-    [Header("References")]
-    public GameObject gameOverCanvas;
+//public class GameOver : MonoBehaviour
+//{
+//    [Header("References")]
+//    public GameObject gameOverCanvas;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.CompareTag("Player"))
-        {
-            ShowGameOver();
-        }
-    }
+//    private void OnCollisionEnter(Collision collision)
+//    {
+//        if (collision.collider.CompareTag("Player"))
+//        {
+//            ShowGameOver();
+//        }
+//    }
 
-    private void ShowGameOver()
-    {
-        if (gameOverCanvas != null)
-        {
-            gameOverCanvas.SetActive(true);
-            Time.timeScale = 0f;
-        }
+//    private void ShowGameOver()
+//    {
+//        if (gameOverCanvas != null)
+//        {
+//            gameOverCanvas.SetActive(true);
+//            Time.timeScale = 0f;
+//        }
  
-    }
+//    }
 
-    public void RestartScene()
-    {
-        Time.timeScale = 1f;
+//    public void RestartScene()
+//    {
+//        Time.timeScale = 1f;
 
-        Scene activeScene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(activeScene.name);
+//        Scene activeScene = SceneManager.GetActiveScene();
+//        SceneManager.LoadScene(activeScene.name);
 
-    }
-}
+//    }
+//}
