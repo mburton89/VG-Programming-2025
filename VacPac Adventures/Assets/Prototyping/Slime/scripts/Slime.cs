c63f262b96556931f3247299f4d79c4b4ad943cb
KeatinWerleman <keatin8756@gmail.com>
2025-09-30 20:05:08 -0500

    fixed flee from mechanic

--- VacPac Adventures/Assets/Prototyping/Slime/scripts/Slime.cs
+++ VacPac Adventures/Assets/Prototyping/Slime/scripts/Slime.cs
@@ -1,15 +1,18 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
 
-    public float slimeSize;
     public float slimeSpeed;
     public float fuelPower;
 
     public Rigidbody rigidBody;
+
+    public Transform activeEnemy;
 
     bool isRelaxed;
 
@@ -33,18 +36,18 @@ public class Slime : MonoBehaviour
     IEnumerator IdleWander()
     {
         Debug.Log("IdleWander" + isRelaxed);
+        StopCoroutine(FleeFromEnemy());
 
         do
         {
             int randX = Random.Range(-5, 5);
-            int randY = Random.Range(5, 20);
+            int randY = Random.Range(8, 12);
             int randZ = Random.Range(-5, 5);
 
             //Pick new Direction (create a new Vector3)
             Vector3 newDirection = new Vector3(randX, randY, randZ);
 
             //Move in new direction (translate in newVector)
-            //transform.Translate(newDirection * slimeSpeed * Time.deltaTime);
             rigidBody.AddForce(newDirection * slimeSpeed, ForceMode.Impulse);
             Debug.Log("AddForce");
 
@@ -61,49 +64,61 @@ public class Slime : MonoBehaviour
         if (other.tag == "Enemy")
         {
             //get transform of other.gameObject and store in variable
+            activeEnemy = other.transform;
 
             if (isRelaxed)
             {
                 isRelaxed = false;
                 //tag offending Enemy with variable to be used in FleeFromEnemy to move away
-
                 StartCoroutine(FleeFromEnemy());
+                print("Starting Flee From Enemy");
             }
             }
     }
 
     IEnumerator FleeFromEnemy()
     {
+        StopCoroutine(IdleWander());
+
         do
         {
 
+
+            float randY = Random.Range(8.0f, 10.0f);
+            float fleeDirectionX = transform.position.x - activeEnemy.position.x;
+            float fleeDirectionZ = transform.position.z - activeEnemy.position.z;
+
             //hop away from enemy
-            //Vector3 directionToFlee = new Vector3(enemy.position.x + transform.position.x, randY, enemy.position.z + transform.position.z);
-            //rigidBody.AddForce(directionToFlee * slimeSpeed / slimeSize, ForceMode.Impulse);
+            //Vector3 directionToFlee = new Vector3(activeEnemy.position.x + transform.position.x, randY, activeEnemy.position.z + transform.position.z);
 
-            //TEMPORARY QUICK HOPS FOR DEBUG
-            int randX = Random.Range(-5, 5);
-            int randY = Random.Range(5, 20);
-            int randZ = Random.Range(-5, 5);
+            Vector3 directionToFlee = new Vector3(fleeDirectionX, randY, fleeDirectionZ);
 
-            //Pick new Direction (create a new Vector3)
-            Vector3 newDirection = new Vector3(randX, randY, randZ);
-
-            //Move in new direction (translate in newVector)
-            //transform.Translate(newDirection * slimeSpeed * Time.deltaTime);
-            rigidBody.AddForce(newDirection * slimeSpeed, ForceMode.Impulse);
+            rigidBody.AddForce(directionToFlee * slimeSpeed, ForceMode.Impulse);
             Debug.Log("Fleeing");
 
-            // cooldown after enemy leaves collision?
-            yield return new WaitForSeconds(1);
-            //if no Enemy in trigger, set isRelaxed to True
+            // time between hops
+            yield return new WaitForSeconds(1.5f);
 
         } while (isRelaxed == false);
 
 
-        // Slimes flee from Enemy (not player)
-        // Enemy triggers Awareness collider -> run FleeFromEnemy for 3 -> check Awareness -> bool isReleaxed
+    }
 
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            StartCoroutine(FleeFromEnemyCooldown());
+            print("Starting FleeFromEnemy Cooldown");
+        }
+    }
+
+    public IEnumerator FleeFromEnemyCooldown()
+    {
+        yield return new WaitForSeconds(8);
+        isRelaxed = true;
+        print("isRelaxed= " + isRelaxed);
+        StartCoroutine(IdleWander());
     }
 
     void StruggleWithVacPac()
