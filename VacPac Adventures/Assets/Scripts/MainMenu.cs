f9a7e71345ba553a7a0e5b83923e578da673cd55
HLandfill-623 <hinc2142@gmail.com>
2025-10-07 20:07:49 -0500

    Main Menu added

--- VacPac Adventures/Assets/Scripts/MainMenu.cs
+++ VacPac Adventures/Assets/Scripts/MainMenu.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public Button mainMenuButton;
+    public KeyCode startKey;
 
     // Start is called before the first frame update
     void Start()
     {
-        mainMenuButton.onClick.AddListener(GameManager.instance.LoadGameplay);
+        mainMenuButton.onClick.AddListener(HandleStartButtonPressed);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(startKey))
+        {
+            HandleStartButtonPressed();
+        }
+    }
+
+    public void HandleStartButtonPressed()
+    {
+        SceneManager.LoadScene(1);
     }
 
 }
